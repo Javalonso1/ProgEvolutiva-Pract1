44fee4a397b63e3af67bd380bf8b4fc8a1368d55
Javalonso1 <javalo08@ucm.es>
2026-02-06 15:08:53 +0100

    codigo antes de la mudanza

--- Pract1/src/App.java
+++ Pract1/src/App.java
@@ -1,7 +1,36 @@
+import java.io.*;
+
 public class App {
 
+    int numCameras; // Numero de camaras
+    int rangVision; // Alcance de camaras
+    float angApertura; // Angulo de las camaras (solo si es con codificacion real)
+    boolean[][] map; // Mapa de bools para guardar objetos que bloqueen las camaras
+    int[][] importancia; // Mapa de valor de vigilancia por cada casilla (solo si es codificacion real)
+    boolean modeReal; // Bool que es true si el algoritmo tiene codificación real y false si es
+                      // binaria
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        LeerData("data1");
+    }
+
+    // Metodo que lee el fichero de la carpeta /data y pasa los valores a las
+    // variables locales
+    public static void LeerData(String filename) {
+        try (FileReader fr = new FileReader("./data/" + filename + ".txt")) {
+            BufferedReader br = new BufferedReader(fr);
+            // Lectura del fichero
+            String linea;
+            /*
+             * while ((linea = br.readLine()) != null)
+             * System.out.println(linea);
+             */
+            br.readLine();
+            linea = br.readLine();
+            System.out.println(linea);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
