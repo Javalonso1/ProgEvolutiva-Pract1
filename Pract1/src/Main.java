b480b34a98d38b87f82f57eb9e726f7886a89cd7
Javalonso1 <123580168+Javalonso1@users.noreply.github.com>
2026-02-10 23:29:49 +0100

    Cambiado a IntelliJ

--- Pract1/src/Main.java
+++ Pract1/src/Main.java
@@ -1,15 +1,54 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.*;
 public class Main {
+    static int numCameras; // Numero de camaras
+    static int rangVision; // Alcance de camaras
+    static float angApertura; // Angulo de las camaras (solo si es con codificacion real)
+    static boolean[][] map; // Mapa de bools para guardar objetos que bloqueen las camaras
+    static int[][] importancia; // Mapa de valor de vigilancia por cada casilla (solo si es codificacion real)
+    static boolean modeReal; // Bool que es true si el algoritmo tiene codificación real y false si es binaria
+
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+        /*
         System.out.printf("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
+
+         */
+        LeerData("data1");
+    }
+
+
+    public static void LeerData(String filename) {
+        try (FileReader fr = new FileReader("./data/" + filename + ".txt")) {
+            BufferedReader br = new BufferedReader(fr);
+            // Lectura del fichero
+            String linea;
+            br.readLine();
+            linea = br.readLine();
+            numCameras = Integer.parseInt(linea);
+            br.readLine();
+            linea = br.readLine();
+            rangVision = Integer.parseInt(linea);
+            br.readLine();
+            linea = br.readLine();
+            angApertura = Float.parseFloat(linea);
+            br.readLine();
+            linea = br.readLine();
+            int x = Integer.parseInt(linea);
+            linea = br.readLine();
+            int y = Integer.parseInt(linea);
+            map = new boolean[x] [y];
+            for (int i = 0; i < x; i++) {
+                String[] valores = br.readLine().trim().split("\\s+");
+                for (int j = 0; j < y; j++) {
+                    map[i][j] = (valores[j] == "1");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
