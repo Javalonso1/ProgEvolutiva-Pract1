9a83d640290be4c67ff380f981fd0f3af4e7a8d5
Javalonso1 <javalo08@ucm.es>
2026-02-11 15:35:59 +0100

    Cromosomas 2 Electric Boogaloo

--- Pract1/src/Chromosome.java
+++ Pract1/src/Chromosome.java
@@ -1,5 +1,17 @@
 public abstract class Chromosome {
-    float[] data;
-    public abstract float[] getData();
+
+    boolean[] genotipo;
+    double[] fenotipo;
+    public double aptitud;
+    public double puntuacion;
+    public double punt_acumulada;
+
+    public double[] getFenotipo()
+    {
+        calculateFenotipo();
+        return  fenotipo;
+    };
+
     public abstract void initializeRandom();
+    abstract void calculateFenotipo();
 }
--- Pract1/src/ChromosomeBinario.java
+++ Pract1/src/ChromosomeBinario.java
@@ -1,15 +1,37 @@
 public class ChromosomeBinario extends Chromosome{
-    public ChromosomeBinario(int size){
-        data = new float[size];
+
+    int TX;
+    int TY;
+    public ChromosomeBinario(int NumCams, int X, int Y){
+        fenotipo = new double[NumCams * 2];
+        TX = X;
+        TY = Y;
+        genotipo = new boolean[NumCams * (TX + TY)];
     }
 
     public void initializeRandom(){
-        for(int i = 0; i < data.length; i++){
-            data[i] = (int)(Math.random() * 2);
+        for(int i = 0; i < genotipo.length; i++){
+            genotipo[i] = (int)(Math.random() * 2) == 1;
         }
+        calculateFenotipo();
     }
 
-    public float[] getData(){
-        return data;
+    void calculateFenotipo(){
+        for(int i = 0; i < fenotipo.length * 0.5f; i++){
+            int sol = 0;
+            for(int j = 0; j < TX; j++){
+                int aux = 0;
+                if(genotipo[i*(TX+TY)+j]) aux = 1;
+                sol += Math.pow(2, TX-(j+1)) * aux;
+            }
+            fenotipo[i*2] = sol;
+            sol = 0;
+            for(int j = 0; j < TY; j++){
+                int aux = 0;
+                if(genotipo[i*(TX+TY)+j]) aux = 1;
+                sol += Math.pow(2, TY-(j+1)) * aux;
+            }
+            fenotipo[i*2 + 1] = sol;
+        }
     }
 }
