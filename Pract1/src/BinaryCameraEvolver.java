20514ecad5c8c9d52934b9c3a6cd5e14eacbf96c
Javalonso1 <javalo08@ucm.es>
2026-02-13 15:06:02 +0100

    los cromosomas sexan

--- Pract1/src/BinaryCameraEvolver.java
+++ Pract1/src/BinaryCameraEvolver.java
@@ -23,7 +23,42 @@ public class BinaryCameraEvolver extends GeneticManager{
 
     @Override
     protected Chromosome[] crossover(Chromosome[] pop) {
-        return new Chromosome[0];
+        Chromosome[] sol = new Chromosome[pop.length];
+        int cifX = 1;
+        int cifY = 1;
+        int aux = map.length;
+        while (aux > 1){
+            cifX++;
+            aux = aux / 2;
+        }
+        aux = map[0].length;
+        while (aux > 1){
+            cifY++;
+            aux = aux / 2;
+        }
+        for(int i = 0; i < pop.length; i += 2){
+            sol[i] = new ChromosomeBinario(NCameras, cifX, cifY);
+            sol[i+1] = new ChromosomeBinario(NCameras, cifX, cifY);
+            switch (crossMethod){
+                case MONOPUNTO:
+                    int cru = (int)(Math.random() * pop[i].getGenotipo().length);
+                    sol[i].cruceMonopunto(pop[i], pop[i+1], cru);
+                    sol[i+1].cruceMonopunto(pop[i+1], pop[i], cru);
+                    break;
+                case UNIFORME:
+                    float[] results = new float[pop[i].getGenotipo().length];
+                    for(int j = 0; j < results.length; j++){
+                        results[j] = (float) Math.random();
+                    }
+                    float prob = (float) Math.random();
+                    sol[i].cruceUniforme(pop[i], pop[i+1], prob, results);
+                    sol[i+1].cruceUniforme(pop[i+1], pop[i], prob, results);
+                    break;
+                default:
+                    break;
+            }
+        }
+        return sol;
     }
 
     @Override
