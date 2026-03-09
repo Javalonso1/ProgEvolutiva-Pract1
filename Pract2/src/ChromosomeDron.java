94c4cb004fdc274337818743eadcc2847f931941
Javalonso1 <123580168+Javalonso1@users.noreply.github.com>
2026-03-09 13:05:56 +0100

    Heuristica a medias

--- Pract2/src/ChromosomeDron.java
+++ Pract2/src/ChromosomeDron.java
@@ -4,7 +4,7 @@ public class ChromosomeDron extends Chromosome<Float, Integer>{
     int C;  //Numero de camaras
     int D;  //Numero de drones
     protected int MAX_INSERCIONES = 3;
-    protected int MAX_PERMUTACIONES = 4;
+    protected int MAX_PERMUTACIONES_HEURISTICA = 4;
     public ChromosomeDron(int NumCams, int numDrones){
         fenotipo = new Integer[NumCams + (numDrones-1)];
         C = NumCams;
@@ -112,7 +112,29 @@ public class ChromosomeDron extends Chromosome<Float, Integer>{
             case HEURISTICA:
                 r = Math.random() * 100;
                 if (r < mutationP){
-                    //NO voy a hacer esto ahora
+                    int n = (int)((Math.random() * MAX_PERMUTACIONES_HEURISTICA-1)+2);
+
+                    int[] pos = new int[n];
+                    for(int i = 0; i < n; i++){
+                        pos[i] = (int)(Math.random() *fenotipo.length);
+                        boolean stop = false;
+                        int j = i - 1;
+                        while (j >= 0 && !stop){
+                            if(pos[i] == pos[j]){
+                                i--;
+                                stop = true;
+                            }
+                        }
+                    }
+                    int realval[] = new int[n];
+                    for(int i = 0; i < n; i++) realval[i] = fenotipo[pos[i]];
+
+                    int[][]permutaciones;
+                    permutaciones = createPermutaciones(realval);
+
+                    //Ahora que tenemos las permutaciones tocara evaluar todas las posibilidades. Lo hare luego
+
+
                 }
                 break;
             default:
@@ -120,6 +142,44 @@ public class ChromosomeDron extends Chromosome<Float, Integer>{
         }
     }
 
+
+    private int[][] createPermutaciones(int[] perm){
+        int size = 1;
+        for(int i = 2; i <= perm.length; i++) size = size * i;
+        int[][] sol = new  int[size][perm.length];
+        if(size == 2){
+            sol[0][0] = perm[0];
+            sol[0][1] = perm[1];
+
+            sol[1][0] = perm[1];
+            sol[1][1] = perm[0];
+
+        }
+        else {
+            int[] perm2 = new  int[perm.length -1];
+            for(int i = 1; i < perm.length; i++) perm2[i-1] = perm[i];
+            int[][] aux2 = createPermutaciones(perm2);
+            int l = 0;
+            for(int pos = 0; pos < perm.length; pos++){
+                for(int i = 0; i < aux2.length; i++){
+                    for(int k = 0; k < perm.length; k++){
+                        if(k == pos) sol[l][k] = perm[0];
+                        else{
+                            if(k > pos){
+                                sol[l][k] = aux2[i][k-1];
+                            }
+                            else {
+                                sol[l][k] = aux2[i][k];
+                            }
+                        }
+                    }
+                    l++;
+                }
+            }
+        }
+
+        return sol;
+    }
     @Override
     void cruceMonopunto(Chromosome c1, Chromosome c2, int corte)
     {
