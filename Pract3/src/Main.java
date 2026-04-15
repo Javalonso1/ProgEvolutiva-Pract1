defa63a01698727206b8eecaf111400d6f573514
Javalonso1 <javalo08@ucm.es>
2026-04-15 17:45:07 +0200

    3 mapas

--- Pract3/src/Main.java
+++ Pract3/src/Main.java
@@ -1,15 +1,16 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Main {
     static int ANCHO = 15;
     static  int ALTO = 15;
-    static int[][] map;
+    static List<int[][]> maps;
     public static void main(String[] args) {
       UIclass ui = new UIclass();
         GenerateMap(3000);
-        ui.setMap(map);
+        ui.setMap(maps.get(0));
 
         ui.simulateButton.addActionListener(e -> evolve(ui));
 
@@ -18,7 +19,7 @@ public class Main {
     public static void evolve(UIclass ui)
     {
         GenerateMap(ui.seed());
-        ui.setMap(map);
+        ui.setMap(maps.get(0));
         RoverEvolver ev = new RoverEvolver(ui, 5, false);
         ev.evolve(ui.getGenNumber(), ui.getPopSize(), ui.elitism(),
                 ui.cross(),
@@ -30,20 +31,23 @@ public class Main {
     }
 
     public static void GenerateMap(int semilla){
-        map = new int[ANCHO][ALTO];
-        Random rand = new Random(semilla);
+        for(int k = 0; k < 3; k++){
+            int[][] map =new int[ANCHO][ALTO];
+            Random rand = new Random(semilla + k);
 
-        // 1  muro     2  arena   3  muestra
-        for (int i = 0; i < ALTO; i++) {
-            for (int j = 0; j < ANCHO; j++) {
-                if (i == 0 || i == ALTO - 1 || j == 0 || j == ANCHO - 1) map[i][j] = 1;
-                else  if (rand.nextDouble() < 0.15 && (i != 1 || j != 1)) map[i][j] = 1;
-                else  if (rand.nextDouble() < 0.15 && (i != 1 || j != 1)) map[i][j] = 3;
-                else  if (rand.nextDouble() < 0.08 && (i != 1 || j != 1)) map[i][j] = 2;
+            // 1  muro     2  arena   3  muestra
+            for (int i = 0; i < ALTO; i++) {
+                for (int j = 0; j < ANCHO; j++) {
+                    if (i == 0 || i == ALTO - 1 || j == 0 || j == ANCHO - 1) map[i][j] = 1;
+                    else  if (rand.nextDouble() < 0.15 && (i != 1 || j != 1)) map[i][j] = 1;
+                    else  if (rand.nextDouble() < 0.15 && (i != 1 || j != 1)) map[i][j] = 3;
+                    else  if (rand.nextDouble() < 0.08 && (i != 1 || j != 1)) map[i][j] = 2;
+                }
             }
-        }
         /*
         visitado = new boolean[ALTO][ANCHO];
         visitado[y][x] = true;*/
+            maps.add(map);
+        }
     }
 }
